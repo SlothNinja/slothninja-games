65f33b5a53a3af1aa8950c7f0db54377302b31b5
SlothNinja <slothninja2@gmail.com>
2020-02-11 23:18:40 -0500

    added module support

--- welcome/controller.go
+++ welcome/controller.go
@@ -3,24 +3,22 @@ package welcome
 import (
 	"net/http"
 
-	"bitbucket.org/SlothNinja/slothninja-games/sn/log"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/restful"
 	"github.com/gin-gonic/gin"
-	"go.chromium.org/gae/service/info"
 )
 
 func Index(c *gin.Context) {
-	ctx := restful.ContextFrom(c)
-	log.Debugf(ctx, "Entering welcome#Index")
-	defer log.Debugf(ctx, "Exiting welcome#Index")
+	// ctx := restful.ContextFrom(c)
+	// log.Debugf(ctx, "Entering welcome#Index")
+	// defer log.Debugf(ctx, "Exiting welcome#Index")
 
 	// if cu, gu := user.CurrentFrom(ctx), user.GUserFrom(ctx); cu == nil && gu != nil {
 	// 	c.Redirect(http.StatusSeeOther, "/user/new")
 	// } else {
 	// 	log.Debugf(ctx, "cu: %#v", cu)
 	c.HTML(http.StatusOK, "welcome/index", gin.H{
-		"VersionID": info.VersionID(ctx),
+		// "VersionID": info.VersionID(ctx),
 		// "CUser":     cu,
-		"Context": ctx})
+		// "Context": ctx})
+		"Context": c})
 	// }
 }
--- welcome/go.mod
+++ welcome/go.mod
@@ -1,14 +1,3 @@
 module github.com/SlothNinja/slothninja-games/welcome
 
-require (
-	bitbucket.org/SlothNinja/gin-render v0.0.0-20190401221903-9110eb7fea98 // indirect
-	bitbucket.org/SlothNinja/slothninja-games v0.0.0-20180507031352-79fac0855a16
-	bitbucket.org/pkg/inflect v0.0.0-20130829110746-8961c3750a47 // indirect
-	github.com/gin-gonic/gin v1.5.0
-	github.com/luci/gtreap v0.0.0-20161228054646-35df89791e8f // indirect
-	go.chromium.org/gae v0.0.0-20190826183307-50a499513efa
-	go.chromium.org/luci v0.0.0-20200210224117-fdb2e4cfcb7d // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	google.golang.org/appengine v1.6.5 // indirect
-)
+require github.com/gin-gonic/gin v1.5.0
