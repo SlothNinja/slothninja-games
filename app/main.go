fa24d0a1f9b3a47f5b422959cc87065a75a23633
SlothNinja <slothninja2@gmail.com>
2020-02-11 22:39:37 -0500

    module changes

--- app/main.go
+++ app/main.go
@@ -1,23 +1,10 @@
-package application
+package main
 
 import (
-	"net/http"
-	_ "net/http/pprof"
-
-	"bitbucket.org/SlothNinja/atf"
-	"bitbucket.org/SlothNinja/confucius"
-	"bitbucket.org/SlothNinja/got"
-	"bitbucket.org/SlothNinja/indonesia"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/game"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/rating"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/restful"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/send"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/type"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/user"
-	"bitbucket.org/SlothNinja/slothninja-games/sn/user_controller"
-	"bitbucket.org/SlothNinja/slothninja-games/welcome"
-	"bitbucket.org/SlothNinja/tammany"
+	"bitbucket.org/SlothNinja/restful"
+	"github.com/SlothNinja/slothninja-games/welcome"
 	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 )
@@ -30,53 +17,113 @@ const (
 	rootPath     = "/"
 )
 
-func init() {
+// func main() {
+// 	// if appengine.IsDevAppServer() {
+// 	// 	gin.SetMode(gin.DebugMode)
+// 	// } else {
+// 	// 	gin.SetMode(gin.ReleaseMode)
+// 	// }
+//
+// 	store := cookie.NewStore([]byte("secret123"))
+//
+// 	r := gin.New()
+// 	r.Use(
+// 		// restful.CTXHandler(),
+// 		// restful.TemplateHandler(r),
+// 		// user.GetGUserHandler,
+// 		// user.GetCUserHandler,
+// 		sessions.Sessions("sngsession", store),
+// 	)
+//
+// 	// Welcome Page (index.html) route
+// 	// welcome.AddRoutes(r)
+// }
+
+func main() {
 	if appengine.IsDevAppServer() {
 		gin.SetMode(gin.DebugMode)
 	} else {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	store := sessions.NewCookieStore([]byte("secret123"))
+	store := cookie.NewStore([]byte("secret123"))
 
 	r := gin.New()
 	r.Use(
 		restful.CTXHandler(),
 		restful.TemplateHandler(r),
-		user.GetGUserHandler,
-		user.GetCUserHandler,
+		//		user.GetGUserHandler,
+		//		user.GetCUserHandler,
 		sessions.Sessions("sngsession", store),
 	)
+	// 	r := gin.Default()
+	// 	store := cookie.NewStore([]byte("secret"))
+	// 	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/hello", func(c *gin.Context) {
+		session := sessions.Default(c)
+
+		if session.Get("hello") != "world" {
+			session.Set("hello", "world")
+			session.Save()
+		}
+
+		c.JSON(200, gin.H{"hello": session.Get("hello")})
+	})
 
 	// Welcome Page (index.html) route
 	welcome.AddRoutes(r)
 
-	// Mail route
-	send.AddRoutes(mailPrefix, r)
-
-	// Games Routes
-	game.AddRoutes(gamesPrefix, r)
-
-	// User Routes
-	user_controller.AddRoutes(userPrefix, r)
-
-	// Rating Routes
-	rating.AddRoutes(ratingPrefix, r)
-
-	// After The Flood
-	atf.Register(gType.ATF, r)
-
-	// Guild of Thieves
-	got.Register(gType.GOT, r)
-
-	// Tammany Hall
-	tammany.Register(gType.Tammany, r)
-
-	// Indonesia
-	indonesia.Register(gType.Indonesia, r)
-
-	// Confucius
-	confucius.Register(gType.Confucius, r)
-
-	http.Handle(rootPath, r)
+	r.Run()
 }
+
+// func init() {
+// 	if appengine.IsDevAppServer() {
+// 		gin.SetMode(gin.DebugMode)
+// 	} else {
+// 		gin.SetMode(gin.ReleaseMode)
+// 	}
+//
+// 	store := cookie.NewStore([]byte("secret123"))
+//
+// 	r := gin.New()
+// 	r.Use(
+// 		restful.CTXHandler(),
+// 		restful.TemplateHandler(r),
+// 		user.GetGUserHandler,
+// 		user.GetCUserHandler,
+// 		sessions.Sessions("sngsession", store),
+// 	)
+//
+// 	// Welcome Page (index.html) route
+// 	welcome.AddRoutes(r)
+//
+// 	// Mail route
+// 	send.AddRoutes(mailPrefix, r)
+//
+// 	// Games Routes
+// 	game.AddRoutes(gamesPrefix, r)
+//
+// 	// User Routes
+// 	user_controller.AddRoutes(userPrefix, r)
+//
+// 	// Rating Routes
+// 	rating.AddRoutes(ratingPrefix, r)
+//
+// 	// After The Flood
+// 	atf.Register(gType.ATF, r)
+//
+// 	// Guild of Thieves
+// 	got.Register(gType.GOT, r)
+//
+// 	// Tammany Hall
+// 	tammany.Register(gType.Tammany, r)
+//
+// 	// Indonesia
+// 	indonesia.Register(gType.Indonesia, r)
+//
+// 	// Confucius
+// 	confucius.Register(gType.Confucius, r)
+//
+// 	http.Handle(rootPath, r)
+// }
